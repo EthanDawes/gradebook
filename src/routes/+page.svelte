e8766ea1def1ddec03f4d65b8f1b125e6a27967b
Ethan Dawes <ethand.python@gmail.com>
2025-05-28 20:03:50 +0300

    Update header when curve changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-    import { gradeStore } from '$lib/stores.svelte.js';
-    import Sidebar from '$lib/components/Sidebar.svelte';
-    import CourseHeader from '$lib/components/CourseHeader.svelte';
-    import GradeCutoffs from '$lib/components/GradeCutoffs.svelte';
-    import GradesTable from '$lib/components/GradesTable.svelte';
-    import GradeStats from '$lib/components/GradeStats.svelte';
-    import EmptyState from '$lib/components/EmptyState.svelte';
+    import { gradeStore } from "$lib/stores.svelte.js";
+    import Sidebar from "$lib/components/Sidebar.svelte";
+    import CourseHeader from "$lib/components/CourseHeader.svelte";
+    import GradeCutoffs from "$lib/components/GradeCutoffs.svelte";
+    import GradesTable from "$lib/components/GradesTable.svelte";
+    import GradeStats from "$lib/components/GradeStats.svelte";
+    import EmptyState from "$lib/components/EmptyState.svelte";
 </script>
 
 <div class="flex h-screen">
@@ -15,7 +15,10 @@
     <div class="flex-1 p-6">
         {#if gradeStore.selectedCourse}
             {@const courseItem = gradeStore.selectedCourse}
-            {@const currentGrade = gradeStore.calculateCourseGrade(courseItem)}
+            {@const currentGrade = gradeStore.calculateCourseGrade(
+                courseItem,
+                courseItem.curve, // Hack to recalculate if curve changes
+            )}
 
             <CourseHeader {courseItem} {currentGrade} />
             <GradeCutoffs {courseItem} />
